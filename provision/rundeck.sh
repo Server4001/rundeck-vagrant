e157aaac682f9888280e59b18abb2579227bddb5
Brice Bentler <server4001@gmail.com>
2016-12-18 23:37:59 -0800

    Adds configuring of rundeck

--- provision/rundeck.sh
+++ provision/rundeck.sh
@@ -4,6 +4,12 @@
 yum install -y java-1.7.0
 rpm -Uvh http://repo.rundeck.org/latest.rpm
 yum install -y rundeck
+
+# Configure Rundeck.
+cp /vagrant/config/rundeck/framework.properties /etc/rundeck/framework.properties
+cp /vagrant/config/rundeck/rundeck-config.properties /etc/rundeck/rundeck-config.properties
+chown rundeck: /etc/rundeck/{framework.properties,rundeck-config.properties}
+chmod 0640 /etc/rundeck/{framework.properties,rundeck-config.properties}
 service rundeckd start
 chkconfig rundeckd on
 
@@ -20,3 +26,5 @@ cp /vagrant/config/hosts/hosts.rundeck /etc/hosts
 cp /vagrant/config/bash/vagrant.bashrc /home/vagrant/.bashrc
 cp /vagrant/config/bash/root.bashrc /root/.bashrc
 chown vagrant: /home/vagrant/.bashrc
+
+# Can login to rundeck at: dev.rundeck.loc:4440 with either user:user or admin:admin.
